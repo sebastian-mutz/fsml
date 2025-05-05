b60c796e681f0dbb45db6fa197d3a6d7464e5ad7
Sebastian G. Mutz <sebastian@mutz.science>
2025-05-05 18:31:19 +0200

    added ford-doc equations for sts module

--- src/fsml_sts.f90
+++ src/fsml_sts.f90
@@ -32,6 +32,10 @@ pure function f_sts_mean(x) result(mean)
 
 ! ==== Description
 !! Computes arithmetic mean.
+!! $$ \bar{x} = \frac{1}{n} \sum_{i=1}^{n} x_i $$
+!! where \( n \) is the size of (or number of observations in) vector x,
+!! \( x_i \) are individual elements in x, and
+!! \( \bar{x} \) is the arithmetic mean of x.
 
 ! ==== Declarations
   real(wp), intent(in) :: x(:)   !! x vector (assumed size array)
@@ -49,6 +53,10 @@ pure function f_sts_var(x) result(var)
 
 ! ==== Description
 !! Computes variance.
+!! $$ \operatorname{var}(x) = \frac{1}{n} \sum_{i=1}^{n} (x_i - \bar{x})^2 $$
+!! where \( n \) is the size of (or number of observations in) vector x,
+!! \( x_i \) are individual elements in x, and
+!! \( \bar{x} \) is the arithmetic mean of x.
 
 ! ==== Declarations
   real(wp), intent(in) :: x(:)   !! x vector (assumed size array)
@@ -66,6 +74,8 @@ pure function f_sts_std(x) result(std)
 
 ! ==== Description
 !! Computes standard deviation.
+!! $$ \sigma = \sqrt{\operatorname{var}(x)} $$
+!! where \( \operatorname{var}(x) \) is the variance of vector x.
 
 ! ==== Declarations
   real(wp), intent(in) :: x(:)   !! x vector (assumed size array)
@@ -83,6 +93,10 @@ pure function f_sts_cov(x,y) result(cov)
 
 ! ==== Description
 !! Computes covariance. x and y must be the same size.
+!! $$ \operatorname{cov}(x, y) = \frac{1}{n} \sum_{i=1}^{n} (x_i - \bar{x})(y_i - \bar{y}) $$
+!! where \( n \) is the size of (or number of observations in) vectors x and y,
+!! \( x_i \) and \( y_i \) are individual elements in x and y, and
+!! \( \bar{x} \) and \( \bar{y} \) are the arithmetic means of x and y.
 
 ! ==== Declarations
   real(wp), intent(in) :: x(:)   !! x vector (assumed size array)
@@ -101,6 +115,10 @@ pure function f_sts_trend(x,y) result(trend)
 
 ! ==== Description
 !! Computes regression coefficient/trend. x and y must be the same size.
+!! $$ m = \frac{\operatorname{cov}(x, y)}{\operatorname{var}(x)} $$
+!! where \( m \) is the slope of the regression line (linear trend),
+!! \( \operatorname{cov}(x, y) \) is the covariance of x and y, and
+!! \( \operatorname{var}(x) \) is the variance of x.
 
 ! ==== Declarations
   real(wp), intent(in) :: x(:)   !! x vector (assumed size array)
@@ -119,6 +137,10 @@ pure function f_sts_corr(x,y) result(corr)
 
 ! ==== Description
 !! Computes Pearson correlation coefficient. x and y must be the same size.
+!! $$ \rho_{x,y} = \frac{\operatorname{cov}(x, y)}{\sigma_x \sigma_y} $$
+!! where \( \rho_{x,y} \) is the Pearson correlation coefficient for vectors x and y,
+!! \( \operatorname{cov}(x, y) \) is the covariance of x and y, and
+!! \( \sigma_{x} \) and \( \sigma_{y} \) are the standard deviations of x and y.
 
 ! ==== Declarations
   real(wp), intent(in) :: x(:)   !! x vector (assumed size array)
