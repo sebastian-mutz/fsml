f4e03bb7b065984611d500555faab9c8ad5680b9
Sebastian <sebastian@mutz.science>
2025-07-11 16:39:40 +0100

    extensive FORD comments moved to interfaces

--- src/fsml.f90
+++ src/fsml.f90
@@ -319,31 +319,152 @@ end interface
 
 ! 1 sample t-test
 interface fsml_ttest_1sample
+  !! The 1-sample t-test determines if the sample mean has the value specified in the null hypothesis.
+  !!
+  !! The null hypothesis \( H_{0} \) and alternative hypothesis \( H_{1} \) can be written as:
+  !! \( H_{0} \): \( \bar{x}  =  \mu_0 \), and \( H_{1} \): \( \bar{x} \neq \mu_0 \)
+  !!
+  !! The test statstic \( t \) is calculated as follows:
+  !! $$ t = \frac{\bar{x} - \mu_0}{s / \sqrt{n}}$$
+  !! where \( \bar{x} \) is the sample mean,
+  !! \( s \) is the sample standard deviation,
+  !! \( n \) is the sample size, and
+  !! \( \mu_0 \) is the population mean.
+  !!
+  !! The degrees of freedom \( \nu \) is calculated as follows:
+  !! $$ \nu = n -1 $$
   module procedure s_tst_ttest_1s
 end interface
 
 ! paired sample t-test
 interface fsml_ttest_paired
+  !! The paired sample t-test (or  dependent sample t-test) determines if
+  !! the mean difference between two sample sets are zero.
+  !! It is mathematically equivalent to the 1-sample t-test conducted
+  !! on the difference vector \( d \) with \( \mu_0 = 0 \).
+  !!
+  !! The null hypothesis \( H_{0} \) and alternative hypothesis \( H_{1} \) can be written as:
+  !! \( H_{0} \): \( \bar{d}  =  0 \), and \( H_{1} \): \( \bar{d} \neq 0 \)
+  !!
+  !! The test statstic \( t \) is calculated as follows:
+  !! $$ t = \frac{\bar{d} - 0}{s_d / \sqrt{n}}$$
+  !! where \( \bar{d} \) is the mean of the differences between the sample sets,
+  !! \( s_d \) is the standard deviation of the differences, and
+  !! \( n \) is the number of paired samples.
+  !!
+  !! The degrees of freedom \( \nu \) is calculated as follows:
+  !! $$ \nu = n -1 $$
   module procedure s_tst_ttest_paired
 end interface
 
 ! 2 sample t-test
 interface fsml_ttest_2sample
+  !! The 2-sample t-test determines if two population means \( \mu_1 \) and \( \mu_2\) are the same.
+  !! The procedure can handle 2-sample t-tests for equal variances and Welch's t-tests for unequal variances.
+  !!
+  !! The null hypothesis \( H_{0} \) and alternative hypothesis \( H_{1} \) can be written as:
+  !! \( H_{0} \): \( \mu_1 \ = \mu_2 \), and \( H_{1} \): \( \mu_1 \ \neq \mu_2\)
+  !!
+  !! The procedure defaults to Welch's t-test for unequal variances if `eq_var` is not specified.
+  !! In this case, the test statstic \( t \) is calculated as follows:
+  !! $$t = \frac{\bar{x}_1 - \bar{x}_2}{\sqrt{\frac{s^2_1}{n_1} + \frac{s^2_2}{n_2} }} $$
+  !! where \( \bar{x}_1 \) and \( \bar{x}_2 \) are the sample means
+  !! \( s^2_1 \) and \( s^2_2 \) are the sample standard deviations, and
+  !! \( n_1 \) and \( n_1 \) are the sample sizes.
+  !! The degrees of freedom \( \nu \) is approximated with the Welch–Satterthwaite equation:
+  !! $$ \nu = \frac{\left( \frac{s_1^2}{n_1} + \frac{s_2^2}{n_2} \right)^2} {\frac{\left( \frac{s_1^2}{n_1} \right)^2}{n_1 - 1} + \frac{\left( \frac{s_2^2}{n_2} \right)^2}{n_2 - 1}} $$
+  !!
+  !! If variances are assumed to be equal (`eq_var = .true.`),
+  !! the procedure conducts a 2 sample t-test for equal variances, using the pooled standard
+  !! deviation \( s_p \) to calculate the t-statistic:
+  !! $$ t = \frac{\bar{x}_1 - \bar{x}_2}{s_p \sqrt{\frac{1}{n_1} + \frac{1}{n_2}}} $$
+  !!
+  !! In case of assumed equal variances, the degrees of freedom is calculated as follows:
+  !! $$ \nu = n_1 + n_2 - 2 $$
   module procedure s_tst_ttest_2s
 end interface
 
 ! Wilcoxon 1-sample signed rank test
 interface fsml_signedrank_1sample
+  !! The 1-sample Wilcoxon signed rank test is a non-parametric test that
+  !! determines if data comes from a symmetric population with centre \( mu_0 \).
+  !! It can be regarded as a non-parametric version of the 1-sample t-test.
+  !! The test statistic \( W \) is the smaller of the sum of positive and
+  !! negative signed ranks:
+  !! $$ W = \min \left( \sum_{d_i > 0} R_i, \sum_{d_i < 0} R_i \right) $$
+  !!
+  !!
+  !! If the data consists of independent and similarly distributed samples
+  !! from distribution \( D \), the null hypothesis \( H_0 \) can be expressed as:
+  !!
+  !! \( D \) is symmetric around \( \mu = \mu_0 \).
+  !!
+  !! The default alternative hypothesis \( H_1 \) is two-sided and also be
+  !! set explicitly (`h1 = "two"`). It can be expressed as:
+  !!
+  !! \( D \) is symmetric around \( \mu \neq \mu_0 \)
+  !!
+  !! If the alternative hypothesis is set to "greater than" (`h1 = "gt"`), it is:
+  !!
+  !! \( D \) is symmetric around \( \mu > \mu_0 \)
+  !!
+  !! If the alternative hypothesis is set to "less than" (`h1 = "lt"`), it is:
+  !!
+  !! \( D \) is symmetric around \( \mu < \mu_0 \)
+  !!
+  !! The procedure takes into consideration tied ranks.
   module procedure s_tst_signedrank_1s
 end interface
 
 ! Wilcoxon 2-sample (paired sample) signed rank test
 interface fsml_signedrank_paired
+  !! The Wilcoxon signed rank test is a non-parametric test that determines
+  !! if two related paired samples come from the same distribution.
+  !! It can be regarded as a non-parametric version of the paired t-test.
+  !!
+  !! The Wilcoxon signed rank test is mathematically equivalent to the
+  !! 1-sample Wilcoxon signed rank test conducted on the difference vector
+  !! \( d = x_1 - x_2 \) with \( mu_0 \) set to zero. Consequently, the
+  !! the null hypothesis \( H_0 \)  can be expressed as:
+  !!
+  !! Samples \( x_1 - x_2 \) are symmetric around \( \mu = 0 \).
+  !!
+  !! The default alternative hypothesis \( H_1 \) is two-sided and also be
+  !! set explicitly (`h1 = "two"`). It can be expressed as:
+  !!
+  !! Samples \( x_1 - x_2 \) are symmetric around \( \mu \neq 0 \)
+  !!
+  !! If the alternative hypothesis is set to "greater than" (`h1 = "gt"`), it is:
+  !!
+  !! Samples \( x_1 - x_2 \) are symmetric around \( \mu > 0 \)
+  !!
+  !! If the alternative hypothesis is set to "less than" (`h1 = "lt"`), it is:
+  !!
+  !! Samples \( x_1 - x_2 \) are symmetric around \( \mu < 0 \)
+  !!
+  !! The procedure takes into consideration tied ranks.
   module procedure s_tst_signedrank_2s
 end interface
 
 ! Wilcoxon rank-sum / Mann–Whitney U test
 interface fsml_ranksum
+  !! The ranks sum test (Wilcoxon rank-sum test or Mann–Whitney U test) is a
+  !! non-parametric test to determine if two independent samples \( x_1 \) and
+  !! \( x_2 \) are have the same distribution. It can be regarded as the non-parametric
+  !! equivalent of the 2-sample t-test.
+  !!
+  !! The Mann–Whitney U statistic is calculated for each sample as follows:
+  !! $$ U_i = R_i - \frac{n_i \cdot (n_i + 1)}{2} $$
+  !! where \( R_i \) is the sum of ranks of sample set \( i \)
+  !! and \( n_i \) is the sample size of sample set \( i \).
+  !! The final U statistic is:
+  !! $$ U = \min(U_1, U_2) $$
+  !!
+  !! The null hypothesis \( H_{0} \) and alternative hypothesis \( H_{1} \) can be written as:
+  !! \( H_0 \): the distributions of \( x_1 \) and \( x_2 \) are equal.
+  !! \( H_1 \): the distributions of \( x_1 \) and \( x_2 \) are not equal.
+  !!
+  !! The procedure takes into consideration tied ranks.
   module procedure s_tst_ranksum
 end interface
 
@@ -353,6 +474,9 @@ end interface
 
 ! ranks elements in an array
 interface fsml_rank
+  !! Ranks all samples such that the smallest value obtains rank 1
+  !! and the largest rank n. Handles tied ranks and assigns average
+  !! rank to tied elements within one group of tied elements.
   module procedure s_utl_rank
 end interface
 
@@ -362,6 +486,7 @@ end interface
 
 ! read csv file into dataframe
 interface fsml_read_csv
+  !! Read CSV file directly into dataframe.
   module procedure s_dat_read_csv
 end interface
 
